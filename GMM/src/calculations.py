5a2edb5699be299682443085d59ae1eeb6a84729
Moiseeva Anastasia <nast.mois@gmail.com>
2014-12-14 05:19:30 +0300

    def getNewMeans on 3D arrays

--- GMM/src/calculations.py
+++ GMM/src/calculations.py
@@ -5,10 +5,9 @@ import sys
 __author__ = 'annie'
 
 '''
-T1 = 3991
-
 D = 39
 M = 512
+T1 ~ 4000-7000
 
 ____________________________
 features_bin T*D
@@ -19,10 +18,10 @@ T2
 T3
 T4
 ...
-T3991
+TN
 ----------------------------
 ____________________________
-ubm D*M
+ubm M*D
 ----------------------------
    D1 D2 D3 ... D39
 M1
@@ -41,7 +40,7 @@ T2
 T3
 T4
 ...
-T3991
+TN
 
 '''
 
@@ -62,20 +61,18 @@ def getGamma(ubm, features):
 
 
 def getLogGMM(ubm, features):
-    sys.stdout.write('\tGet LogGMM\n'); sys.stdout.flush()
+    print('\tGet LogGMM')
     f_gmm = getF_GMM(ubm, features)
     return np.log(np.sum(f_gmm, axis=1))
 
 
 def getNewMeans(ubm, features, r=20):
-    sys.stdout.write('\tGet new means\n'); sys.stdout.flush()
+    print('\tGet new means')
     gamma = getGamma(ubm, features)
-    f_s = np.empty((ubm.means.shape))
-    featuresT = features.T
-    for m in xrange(ubm.numberGauss):
-        f_s[m] = np.sum(featuresT*gamma[:, m], axis=1)
+    statisticsFirst = np.sum(np.concatenate((features[np.newaxis],)*ubm.numberGauss)
+                      * np.concatenate((gamma.T[:, :, np.newaxis],)*ubm.dim, axis=2), axis=1)
     n_plus_r = np.sum(gamma) + r
-    newMeans = 1 / n_plus_r * f_s + r / n_plus_r * ubm.means
+    newMeans = 1 / n_plus_r * statisticsFirst + r / n_plus_r * ubm.means
     return newMeans
 
 
