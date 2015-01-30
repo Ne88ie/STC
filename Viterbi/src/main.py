7c2ff306c134bab6745de770feac08921b511f4c
Moiseeva Anastasia <nast.mois@gmail.com>
2015-01-30 06:26:23 +0300

    add log in v

--- Viterbi/src/main.py
+++ Viterbi/src/main.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from __future__ import print_function, division
 __author__ = 'moiseeva'
 
 import os
@@ -6,7 +8,7 @@ import numpy as np
 from struct import pack, unpack
 from GMM.src.IOData import getGmm, getFeatures
 
-PI = (0.5, 0.5) # априорные вероятности начального состояния
+PI = 0.5 # априорная вероятность начального состояния
 A = np.array([[0.95, 0.05], [0.05, 0.95]]) # матрица переходов
 
 
@@ -16,9 +18,9 @@ class Viterbi:
         self.gmms = []
         for path in pathes_to_gmms:
             self.gmms.append(getGmm(path))
-        self.number_of_speakers = len(pathes_to_gmms)
-        self.len = self.features.shape[0]
-        self.dim = self.features.shape[1]
+        self.number_of_speakers = len(pathes_to_gmms) # K
+        self.len = self.features.shape[0] # T
+        self.dim = self.features.shape[1] # D
         self.sys = np.empty(self.len, dtype=np.int)
 
     def get_distribution_of_hidden_states(self, x_t):
@@ -33,7 +35,8 @@ class Viterbi:
             gmm.covMatDiag = 1 / gmm.covMatDiag
             buf = 0
             for i in range(gmm.numberGauss):
-                buf += gmm.weightGauss[i] * np.exp(np.sum(-0.5 * np.power(x_t - gmm.means[i], 2) * gmm.covMatDiag[i]))
+                dif = x_t - gmm.means[i]
+                buf += gmm.weightGauss[i] * np.exp(np.sum(-0.5 * dif ** 2 * gmm.covMatDiag[i]))
             res.append(buf)
         return res
 
@@ -45,24 +48,22 @@ class Viterbi:
     def save_sys(self, path):
         with open(path, 'wb') as f:
             f.write(pack('<i', self.sys.size))
-            f.write(pack('<{0}i'.format(self.sys.size), *self.sys.ravel())) # ravel?
+            f.write(pack('<{0}i'.format(self.sys.size), *self.sys)) # ravel?
 
     def run(self):
-        v = np.empty((self.len, self.number_of_speakers), dtype=np.float)
-        prevs = np.empty((self.len, self.number_of_speakers), dtype=np.int)
-        v[0] = self.get_distribution_of_hidden_states(self.features[0])
-        v[0, 0] *= PI[0]
-        v[0, 1] *= PI[1]
+        v = np.empty((self.len, self.number_of_speakers), dtype=np.float) # T * K
+        prevs = np.empty((self.len, self.number_of_speakers), dtype=np.int) # T * K
+        v[0] = np.log(self.get_distribution_of_hidden_states(self.features[0])) + np.log(PI) # 1 * K
         print('... processing', end='')
         for t, features in enumerate(self.features[1:], start=1):
-            v[t] = self.get_distribution_of_hidden_states(features)
+            v[t] = np.log(self.get_distribution_of_hidden_states(features))
             for k in range(self.number_of_speakers):
-                jumps = (A[0, k]*v[t-1, 0], A[1, k]*v[t-1, 1])
+                jumps = (np.log(A[0, k]) + v[t-1, 0], np.log(A[1, k]) + v[t-1, 1])
                 arg = int(np.argmax(jumps))
-                v[t, k] *= jumps[arg]
+                v[t, k] += jumps[arg]
                 prevs[t, k] = arg
 
-            print('\r... processing {0:.2%}'.format((t+1)/self.len), end='')
+            print('\r... processing {0:.2%}'.format((t+1.0)/self.len), end='')
         print('\r' + ' '*30 + '\r', end='')
 
         start = int(np.argmax(v[-1]))
@@ -70,36 +71,36 @@ class Viterbi:
         return self.sys
 
     @classmethod
-    def get_ref(cls, path):
+    def get_indx(cls, path):
         with open(path, 'rb') as f:
             amt = unpack('<i', f.read(4))[0]
             return np.array(unpack('<{0}i'.format(amt), f.read(4*amt)), dtype=np.int)
 
     @classmethod
-    def check(cls, path_to_ref, path_to_sys=None, sys=None):
-        if not sys:
-            sys = cls.get_ref(path_to_sys)
-        ref = cls.get_ref(path_to_ref)
+    def check(cls, path_to_ref, sys):
+        if type(sys) == str:
+            sys = cls.get_indx(sys)
+        ref = cls.get_indx(path_to_ref)
         tp = np.count_nonzero(np.bitwise_xor(ref, sys) < 1)
         prc = tp/(sys.size or 0.0001)
         return prc
 
-def test(path_to_dir):
+def test(path_to_dir, path_to_dir_sys):
     """
     :param path_to_dir: path to input data.
     :return:
     START
-    File: fafop, PRC = 10.66%
-    File: fafgm, PRC = 64.17%
-    File: fabrl, PRC = 64.33%
-    File: faawt, PRC = 58.25%
-    File: fadxv, PRC = 31.92%
-    File: fadxy, PRC = 68.08%
-    File: faawu, PRC = 50.66%
-    File: faawn, PRC = 28.47%
-    File: faawe, PRC = 48.45%
-    Average PRC = 47.22%
-    EXECUTED FOR 53m 35s
+    File: fabrl, PRC = 72.54%
+    File: fafop, PRC = 97.58%
+    File: fafgm, PRC = 84.23%
+    File: faawt, PRC = 89.64%
+    File: faawu, PRC = 73.41%
+    File: fadxy, PRC = 85.05%
+    File: faawn, PRC = 85.38%
+    File: faawe, PRC = 79.92%
+    File: fadxv, PRC = 91.22%
+    Average PRC = 84.33%
+    EXECUTED FOR 73m 33s
     """
     start = time.time()
     print('START')
@@ -116,7 +117,7 @@ def test(path_to_dir):
             path_to_features = path + '.features_bin'
             pathes_to_gmms = [path + '.1.gmm', path + '.2.gmm']
             path_to_ref = path + '.ref.indx'
-            path_to_sys = os.path.join('C:/Users/moiseeva/PycharmProjects/data_kud_lab3/res', name + '.sys.indx')
+            path_to_sys = os.path.join(path_to_dir_sys, name + '.sys.indx')
             vit = Viterbi(path_to_features, pathes_to_gmms)
             vit.run()
             vit.save_sys(path_to_sys)
@@ -146,8 +147,9 @@ def check(dir_ref, dir_sys):
 
 
 if __name__ == '__main__':
-    path_to_base = 'C:/Users/moiseeva/PycharmProjects/data_kud_lab3/base/'
+    path_to_base = '/Users/annie/SELabs/Kudashev/lab3/base'
+    path_to_dir_sys = '/Users/annie/SELabs/Kudashev/lab3/res'
     dir_romanenko = 'C:/Users/moiseeva/PycharmProjects/data_kud_lab3/romanenko'
     dir_my = 'C:/Users/moiseeva/PycharmProjects/data_kud_lab3/res'
-    test(path_to_base)
+    test(path_to_base, path_to_dir_sys)
     # check(path_to_base, dir_romanenko)
